3182496b91ac778be09784351bfcd43d15c26813
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-12-01 01:11:35 -0600

    Bug fix with workers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
+  include ActionController::Cookies
 
-    def hello_world
-      session[:count] = (session[:count] || 0) + 1
-      render json: { count: session[:count] }
-    end
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
 end
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,9 +4,19 @@ import { useState, useEffect } from "react";
 
 
 export default function Home() {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/hello")
+    .then((res) => res.json())
+    .then((data) => setCount(data.count))
+    .catch((error) => console.error("Error fetching data: ", error));
+  }, [])
 
   return (
-    <main></main>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Next.js</h1>
+      <p className="text-lg">Page Count from API: {count}</p>
+    </main>
   );
 }
