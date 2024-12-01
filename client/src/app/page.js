df3705b3e3733d8aa9a2e39690f756e6dbef3ebc
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-30 23:54:02 -0600

    Add Next.js, configure proxy, and begin setting up home component to test frontend/backend interaction

---
+++ client/src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+
+export default function Home() {
+  const [count, setCount] = useState();
+
+  return (
+    <main></main>
+  );
+}
